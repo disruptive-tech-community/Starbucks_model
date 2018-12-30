3ede52b5b191a1deee334d5d6acd7e1e3704c42d
Karvendhan <karvsmech@gmail.com>
2018-12-30 11:54:37 +0530

    File addition: Construction Sample_size_determination

---
+++ sample_size_determination.py
@@ -0,0 +1,22 @@
+# Practical Significance
+
+# import packages
+import numpy as np
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+# Lets say have a baseline web portal click-through rate of 10% and want to
+# see a manipulation increase this baseline to 12%. How many observations would
+# we need in each group in order to detect this change with power  1−𝛽=.80
+# (i.e. detect the 2% absolute increase 80% of the time), at a Type I error
+# rate of  𝛼=.05 ?
+
+# Trial and Error Method:
+# -------------------------
+
+
+
+
+
+
