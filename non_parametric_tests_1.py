79df724db9c5a43e86253e2ea1dc892a0e6a0a4b
Karvendhan <karvsmech@gmail.com>
2018-12-30 21:54:31 +0530

    Feature Addition: Permutuation Tests

--- non_parametric_tests_1.py
+++ non_parametric_tests_1.py
@@ -57,6 +57,66 @@ plt.hist(data['time'], bins = np.arange(0, data['time'].max()+400, 400))
 lims = quantile_ci(data['time'], 0.9)
 print(lims)
 
+# Permutation Tests
+#-------------------
+
+def quantile_permtest(x, y, q, alternative='less', n_trials=10_000):
+    """
+    Compute a confidence interval for a quantile of a dataset using a bootstrap
+    method.
+
+    Input parameters:
+        x: 1-D array-like of data for independent / grouping feature as 0s and 1s
+        y: 1-D array-like of data for dependent / output feature
+        q: quantile to be estimated, must be between 0 and 1
+        alternative: type of test to perform, {'less', 'greater'}
+        n_trials: number of permutation trials to perform
+
+    Output value:
+        p: estimated p-value of test
+    """
+
+    # initialize storage of bootstrapped sample quantiles
+    sample_diffs = []
+
+    # For each trial...
+    for _ in range(n_trials):
+        # randomly permute the grouping labels
+        labels = np.random.permutation(y)
+
+        # compute the difference in quantiles
+        cond_q = np.percentile(x[labels == 0], 100 * q)
+        exp_q = np.percentile(x[labels == 1], 100 * q)
+
+        # and add the value to the list of sampled differences
+        sample_diffs.append(exp_q - cond_q)
+
+    # compute observed statistic
+    cond_q = np.percentile(x[y == 0], 100 * q)
+    exp_q = np.percentile(x[y == 1], 100 * q)
+    obs_diff = exp_q - cond_q
+
+    # compute a p-value
+    if alternative == 'less':
+        hits = (sample_diffs <= obs_diff).sum()
+    elif alternative == 'greater':
+        hits = (sample_diffs >= obs_diff).sum()
+
+    return (hits / n_trials)
+
+
+
+data = pd.read_csv(data_dir + 'permutation_data.csv')
+bin_borders = np.arange(0, data['time'].max()+400, 400)
+plt.hist(data[data['condition'] == 0]['time'], alpha = 0.5, bins = bin_borders)
+plt.hist(data[data['condition'] == 1]['time'], alpha = 0.5, bins = bin_borders)
+plt.legend(labels = ['control', 'experiment']);
+
+print(np.percentile(data[data['condition'] == 0]['time'], 90),
+      np.percentile(data[data['condition'] == 1]['time'], 90))
+
+quantile_permtest(data['time'], data['condition'], 0.9, alternative = 'less')
+
 
 
 
